f49887febc9b882e54595a2bd15c9dbba8e75df9
Thiago Oliveira <tmeloliveira@gmail.com>
2017-08-17 19:31:37 -0300

    Remove coffee files and replace it with JS using new syntax! :tada:

---
+++ source/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+import FastClick from 'fastclick';
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+	}, false);
+}
